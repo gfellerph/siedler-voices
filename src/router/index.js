1c135d0bbd522621724b03531ba3e83ef38414d2
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2017-03-04 15:42:56 +0100

    Bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Sounds from '@/components/Sounds';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+      name: 'Sounds',
+      component: Sounds,
+    },
+  ],
+});
